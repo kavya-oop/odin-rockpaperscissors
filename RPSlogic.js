df4b2ea80790be33fa8ccc4265f5a618979ae185
kavya-oop <194732763+kavya-oop@users.noreply.github.com>
2025-05-16 11:26:09 -0700

    Javascript brute-force rock, paper, scissors game
    Added index.html file and js file with all logic

---
+++ RPSlogic.js
@@ -0,0 +1,88 @@
+/*
+    Pseudocode:
+
+    Things needed:
+        - Computer choice
+        -Player choice
+        -Comparison logic for who wins
+        -Main func to put all pieces together and run game
+*/
+
+//Start scores of both human and computer at 0
+let humanScore = 0;
+let computerScore = 0;
+
+let getComputerChoice = function() {
+    let choice = Math.random() * 3; //choice is between 0 and 3 (non-inclusive)
+
+    if (choice < 1) {
+        return 'rock';
+    } else if (choice < 2){
+        return 'scissors';
+    } else {
+        return 'paper';
+    }
+};
+
+let getHumanChoice = () => prompt("Pick either rock, paper, or scissors: ");
+
+let playRound = function(computerChoice, humanChoice) {
+
+    //REVIEW notes: could have made this more efficient by combining cases
+    humanChoice = humanChoice.toLowerCase();
+    
+    if (humanChoice == 'rock') {
+        if (computerChoice == 'rock') {
+            console.log("Tie!");
+        }
+        else if (computerChoice == 'paper') {
+            console.log("You lose, paper captures rock oop");
+            computerScore++;
+        }
+        else {
+            console.log("You win! Rock crushes scissors");
+            humanScore++;
+        }
+    }
+    if (humanChoice == 'paper') {
+        if (computerChoice == 'paper') {
+            console.log("Tie!");
+        }
+        else if (computerChoice == 'scissors') {
+            console.log("You lose, scissors cuts paper owie");
+            computerScore++;
+        }
+        else {
+            console.log("You win! Paper captures rock!");
+            humanScore++;
+        }
+    }
+    if (humanChoice == 'scissors') {
+        if (computerChoice == 'scissors') {
+            console.log("Tie!");
+        }
+        else if (computerChoice == 'rock') {
+            console.log("You lose, rock crushes scissors ouchie");
+            computerScore++;
+        }
+        else {
+            console.log("You win! Scissors cuts paper whee");
+            humanScore++;
+        }
+    }
+        
+};
+
+let playGame = function() {
+    for(let i = 0; i < 5; i++) {
+        playRound(getComputerChoice(), getHumanChoice());
+    }
+
+    if(humanScore > computerScore) console.log('You win!');
+    else if (humanScore == computerScore) console.log('Tied by RNG');
+    else console.log('Beat by RNG?? Oop.');
+}
+
+
+//run the game finally!
+playGame();
